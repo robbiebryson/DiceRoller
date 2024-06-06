9756824ad91efe7503ee87bbfdfba98dc169a8cc
robbiebryson <65382229+robbiebryson@users.noreply.github.com>
2024-06-05 23:13:37 -0400

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Rob Bryson on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
