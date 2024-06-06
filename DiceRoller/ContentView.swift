dbdde093f1c8797efad68c534a35254f71f17713
robbiebryson <65382229+robbiebryson@users.noreply.github.com>
2024-06-05 23:45:18 -0400

    Complete Dice Roller App
    Start to Finish Dice Roller

--- DiceRoller/ContentView.swift
+++ DiceRoller/ContentView.swift
@@ -7,15 +7,40 @@
 
 import SwiftUI
 
+
+let gradientColors: [Color] = [
+    .gradientTop,
+    .gradientBottom
+]
+
 struct ContentView: View {
+    @State private var numberOfDice: Int = 1
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Dice Roller")
+                .font(.largeTitle.lowercaseSmallCaps())
+            
+            HStack {
+                ForEach(1...numberOfDice, id: \.description) {_ in
+                    DiceView()
+                }
+            }
+            HStack {
+                Button("Remove Dice", systemImage: "minus.circle.fill") {
+                    numberOfDice -= 1
+                }
+                .disabled(numberOfDice == 1)
+                Button("Add Dice", systemImage: "plus.circle.fill") {
+                    numberOfDice += 1
+                }
+                .disabled(numberOfDice == 5)
+            }
+            .padding()
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Gradient(colors: gradientColors))
+        .tint(.white)
     }
 }
 
